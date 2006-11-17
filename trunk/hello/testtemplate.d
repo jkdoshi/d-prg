fe041a17bfc978768ca9aed88f2ccecdfca6b22c
doshi.jitesh <doshi.jitesh@94e0ab2a-6622-0410-bbd0-0b11983eab4e>
2006-11-17 03:46:15 +0000

    Added class and template tests.

---
+++ trunk/hello/testtemplate.d
@@ -0,0 +1,38 @@
+﻿/++
+ + module to test template mechanics.
+ +/
+module testtemplate;
+
+import std.stdio;
+
+/++
+ + Compile-time factorial.
+ + Example:
+ +--------------------------------------
+ + const int y = 10;
+ + int x = factorial!(y);
+ +--------------------------------------
+ +/
+template factorial(int x)
+{
+	const factorial = x * factorial!(x-1);
+}
+
+/// ditto
+template factorial(int x : 1)
+{
+	const factorial = 1;
+}
+
+unittest
+{
+	writefln("UnitTest: testtemplate");
+	const int y = 10;
+	auto x = factorial!(y);
+}
+
+static assert(factorial!(1) == 1);
+static assert(factorial!(2) == 2);
+static assert(factorial!(3) == 6);
+static assert(factorial!(4) == 24);
+static assert(factorial!(5) == 120);
