265d753c742ae2fa1708276cfc88ab058b9b93a5
doshi.jitesh <doshi.jitesh@94e0ab2a-6622-0410-bbd0-0b11983eab4e>
2007-07-12 23:05:03 +0000

    - added mem.d and mixins.d

---
+++ trunk/hello/mem.d
@@ -0,0 +1,54 @@
+﻿/++
+ + Memory managment test module.
+ +/
+module mem;
+
+import std.stdio;
+
+class A
+{
+	public:
+	this(char[] name = "noname") {
+		_name = name;
+		writefln("A.this(%s)", _name);
+	}
+	~this() {
+		writefln("A.~this(%s)", _name);
+	}
+	char[] toString() {
+		return _name;
+	}
+	int opCmp(A other) {
+		if(_name > other._name) {
+			return 1;
+		} else if(_name < other._name) {
+			return -1;
+		} else {
+			return 0;
+		}
+	}
+	private	char[] _name;
+}
+
+class B : public A {
+	public:
+	this(char[] name = "noname") {
+		super(name);
+		writefln("B.this(%s)", this._name);
+	}
+	~this() {
+		writefln("B.~this(%s)", this._name);
+	}
+	char[] toString() {
+		return "B::" ~ super.toString();
+	}
+}
+
+unittest
+{
+	scope A foo = new A;
+	writefln("foo=%s", foo);
+	auto bar = new A("bar");
+	auto lum = new B("lum");
+	writefln("%s wins", bar > lum ? bar : lum);
+}
